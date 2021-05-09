389dae04acdb90fca95690ae61487ddc98af88f1
Sam Coleman <scoleman@olin.edu>
2021-05-09 00:52:06 -0400

    Work on live analysis

--- Live.m
+++ Live.m
@@ -1,11 +1,12 @@
-% This scrip[t facilitates connecting to and collecting data from an
+%% Live Detection
+%Sam Coleman
+%Modified from code provided by Sam Michalka
+
+% This script facilitates connecting to and collecting data from an
 % Arduino and EMG sensors. Check out the Setup and Data Collection document
 % for additional context.
-
-% You will need to set up your command window so that you can easily view
-% outputs from this code.
-
-serialportlist("available") %This outputs the available serial ports
+%% Load Trained Algorithm
+load("data/trainedAlgorithm.mat");
 
 %% Configure serial port 
 clear;  % Clear all variables
@@ -22,34 +23,19 @@ sObj %This spits out the object again.
 % type sObj into the command window. If it is still zero, something is
 % wrong.
 %% Get data and write it to an array
-% You change these things:  
-%EMG.setname = 'red'; % Name of this data collection run
 num_sensors = 3;   % Change this to match your number of sensors!
 numevents = 1; % Must divide evenly by number of eventoptions
 epochlength = 3; % Time in seconds to collect data per trial
-%savedata = true; %True/false flag dependingon if you want to save your data
 waittime = 0.5; % Time in seconds between rock, paper, scissors
 
-%You don't need to change these things:
-%eventoptions = categorical({'rock', 'paper', 'scissors'});
 nbchan = num_sensors; % Number of channels to collect (starts with A0, up to A3)
 srate = 500; % 500 Hz collection rate
-% 
-% % Generate pseudorandom ordering
-% EMG.epochlabelscat = repmat(eventoptions,1,round(numevents./length(eventoptions)));
-% EMG.epochlabelscat = EMG.epochlabelscat(randperm(length(EMG.epochlabelscat))); %Shuffle event order (you can comment this out)
-
-% Create data structure for times and data.
 n=ceil(epochlength .* srate); % Number of time points per event
-%EMG.data = zeros(EMG.nbchan,n,numevents);
-%EMG.alltimes = zeros(n,numevents);
 data = zeros(nbchan, n, 1);
-% Loop through and collect data for each event
 for ev = 1:numevents
     disp("Press any key initiate game");
     pause;
     disp("Move your hand immediately on the word shoot");
-%     disp(strcat("This time, form: ", upper(string(EMG.epochlabelscat(ev)))));
     disp("...");
     pause(waittime);    
     disp("ROCK");
@@ -67,25 +53,28 @@ for ev = 1:numevents
             indata = str2double(split(readline(sObj)));
             if length(indata) ==7 
                 if indata(2) ~= 0
-                    %EMG.alltimes(j,ev) = indata(2);
                     data(:,j,ev) = indata(3:2+nbchan);
                     j = j+1;
                 end
             end
         end
     end
-    %EMG.alltimes(:,ev) = EMG.alltimes(:,ev) - EMG.alltimes(1,ev); % set first time point to zero
 end
-% if savedata
-%     % Create a data directory if it doesn't exist
-%     if not(isfolder("data"))
-%         mkdir("data")
-%     end
-%     % Save the data file with timestamp
-%     save(strcat('data/EMGdata-',EMG.setname))
-% end
+
 
 %% pre-process data
 for channel =1:size(data,1) 
     data(channel,:, :) = data(channel,:, :) - mean(data(channel,:, :),2); 
 end
+
+%% Extract Features
+emg_aac = extract_features.calc_aac(data);
+emg_damv_10 = extract_features.calc_DAMV(data, 10);
+emg_dasdv_10 = extract_features.calc_dasdv(data, 10)';
+emg_max = extract_features.max_value(data);
+features = [emg_aac; emg_damv_10; emg_dasdv_10; emg_max]';
+
+%% Predict
+prediction_label = predict(trainedAlg, features);
+disp("You threw\n");
+disp(prediction_label);
